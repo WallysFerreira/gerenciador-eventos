65dbc965e52597b34d76dde97843bb210085a859
A <>
2023-06-08 09:48:29 -0300

    init commit

---
+++ src/main/java/clt/senac/gerenciadoreventos/GerenciadoreventosApplication.java
@@ -0,0 +1,13 @@
+package clt.senac.gerenciadoreventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadoreventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadoreventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clt/senac/gerenciadoreventos/GerenciadoreventosApplicationTests.java
@@ -0,0 +1,13 @@
+package clt.senac.gerenciadoreventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadoreventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
