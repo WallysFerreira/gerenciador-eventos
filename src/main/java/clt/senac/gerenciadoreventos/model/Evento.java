da909b5ad111ac7edce2a0f9a937b85c02e64cb9
Wallys Ferreira <wallysfsilva@gmail.com>
2023-06-15 09:29:27 -0300

    criar atributos da classe evento

--- src/main/java/clt/senac/gerenciadoreventos/model/Evento.java
+++ src/main/java/clt/senac/gerenciadoreventos/model/Evento.java
@@ -1,7 +1,22 @@
 package clt.senac.gerenciadoreventos.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Calendar;
 
 @Entity
 public class Evento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String titulo;
+    private String descricao;
+    private String local;
+    private int andar;
+    private int quantidade_maxima;
+    private Calendar data;
+    private String responsavel;
 }
